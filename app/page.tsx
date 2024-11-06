cbad311ed98c1400716e0c4b4b958d9a0d528e59
Tim J. Chang <tim.jtchang@gmail.com>
2024-11-05 23:15:58 -0800

    complete wallet

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,96 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+import {
+  createConfig,
+  WagmiProvider,
+  useAccount,
+  useConnect,
+  useDisconnect,
+  useBalance,
+  useSwitchChain,
+  useConfig,
+} from "wagmi";
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+import { mainnet, sepolia } from "wagmi/chains";
+import { http } from "viem";
+import { injected } from "wagmi/connectors";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { formatEther } from "viem";
+
+const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+const url: string = `https://eth-sepolia.g.alchemy.com/v2/${apiKey}`;
+
+console.log(`https://eth-sepolia.g.alchemy.com/v2/${apiKey}`);
+console.log(
+  "https://eth-sepolia.g.alchemy.com/v2/nct9HluWchOoLlVjvG0T0iWwZ2B19wZL"
+);
+
+const config = createConfig({
+  chains: [mainnet, sepolia],
+  transports: {
+    [mainnet.id]: http(),
+    [sepolia.id]: http(url),
+  },
+  connectors: [injected()],
+});
+
+const chains = config.chains;
+
+const queryClient = new QueryClient();
+
+function Profile() {
+  const { address, isConnected } = useAccount();
+  const { connect, connectors } = useConnect();
+  const { disconnect } = useDisconnect();
+  const { chain } = useAccount();
+  const { switchChain } = useSwitchChain();
+
+  const {
+    data: balance,
+    isLoading,
+    isError,
+  } = useBalance({ address: address });
+
+  if (isConnected) {
+    if (isLoading) return <div>Loading balance...</div>;
+    if (isError) return <div>Error fetching balance</div>;
+    return (
+      <div>
+        <div>Connected to {address}</div>
+        <div>
+          Balance:{" "}
+          {balance ? `${formatEther(balance.value)}${balance.symbol}` : "N/A"}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        {chain && <div>Connected to {chain.name}</div>}
+        {[mainnet, sepolia].map((x) => (
+          <div key={x.id}>
+            <button
+              disabled={!switchChain || x.id === chain?.id}
+              onClick={() => switchChain({ chainId: x.id })}
+            >
+              {x.name} {x.id === chain?.id && "(current)"}
+            </button>
+          </div>
+        ))}
+        <button onClick={() => disconnect()}>Disconnect</button>
+      </div>
+    );
+  }
+  return (
+    <button onClick={() => connect({ connector: connectors[0] })}>
+      Connect Wallet
+    </button>
+  );
+}
+
+export default function App() {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+          <Profile />
+        </main>
+      </QueryClientProvider>
+    </WagmiProvider>
   );
 }
